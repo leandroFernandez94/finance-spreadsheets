63bc3fd05b86df8625856dfdc06ce3532f947cda
Leandro Fernandez <leandroofernandezz@gmail.com>
2021-05-22 15:05:48 -0300

    fix build

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,5 @@
 import Head from 'next/head'
 import Layout, { siteTitle } from '../components/layout'
-import utilStyles from '../styles/utils.module.css'
-import { getSortedPostsData } from '../lib/posts'
-import Link from 'next/link'
-import Date from '../components/date'
 
 export default function Home({ allPostsData }) {
   return (
@@ -13,13 +9,4 @@ export default function Home({ allPostsData }) {
       </Head>
     </Layout>
   )
-}
-
-export async function getStaticProps() {
-  const allPostsData = getSortedPostsData()
-  return {
-    props: {
-      allPostsData
-    }
-  }
 }

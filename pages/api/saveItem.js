b672010775685009446670f951fefd8aa8c1ed89
Leandro Fernandez <leandroofernandezz@gmail.com>
2021-05-22 16:54:13 -0300

    clean BE

--- pages/api/saveItem.js
+++ pages/api/saveItem.js
@@ -1,34 +1,30 @@
 import { GoogleSpreadsheet } from 'google-spreadsheet'
 
-function getSpreadSheetIndexByName(name, shared) {
-  if (shared) return 2
-  switch (name) {
-    case 'leandro':
-      return 0
-    case 'jazmin':
-      return 1
-  }
+const LEANDRO_INDEX = 0
+const JAZMIN_INDEX = 1
+const SHARED_INDEX = 2
+
+const spreadsheetIndexByName = {
+  leandro: LEANDRO_INDEX,
+  jazmin: JAZMIN_INDEX,
+  shared: SHARED_INDEX
 }
 
 export default async (req, res) => {
   const data = JSON.parse(req.body)
   try {
-
-    console.log('setting doc')
     const doc = new GoogleSpreadsheet('1eSsbQy6NzhIc7vD3U_GYC3OzZScJpIy7ToTDAqDP1ts');
-    console.log('setting auth')
     await doc.useServiceAccountAuth({
       client_email: process.env.GOOGLE_SHEETS_CLIENT_EMAIL,
       private_key: process.env.GOOGLE_SHEETS_PRIVATE_KEY.replace(/\\n/g, "\n"),
     });
 
-    console.log('loading info')
     await doc.loadInfo(); // loads document properties and worksheets
 
-    const index = getSpreadSheetIndexByName(data.name, data.shared)
+    const index = spreadsheetIndexByName[data.shared ? 'shared' : data.name]
     const sheet = doc.sheetsByIndex[index]
 
-    if (index === 2) {
+    if (index === SHARED_INDEX) {
       await sheet.addRow({
         'quien gasto': data.name,
         descripcion: data.description,
@@ -38,10 +34,12 @@ export default async (req, res) => {
     }
 
     if (data.type === 'entry') {
-      await sheet.addRow({
+      const newRow = {
         ingreso: data.description,
         'valor ingreso': data.value
-      })
+      }
+      console.log('saving row', newRow)
+      await sheet.addRow(newRow)
     } else {
       await sheet.addRow({
         gasto: data.description,

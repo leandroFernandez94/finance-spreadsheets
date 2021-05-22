6e386de0307191cc3fd415b3175409247f9a7b35
Leandro Fernandez <leandroofernandezz@gmail.com>
2021-05-22 16:06:32 -0300

    change regex

--- pages/api/saveItem.js
+++ pages/api/saveItem.js
@@ -16,7 +16,7 @@ export default async (req, res) => {
   const doc = new GoogleSpreadsheet('1eSsbQy6NzhIc7vD3U_GYC3OzZScJpIy7ToTDAqDP1ts');
   await doc.useServiceAccountAuth({
     client_email: process.env.GOOGLE_SHEETS_CLIENT_EMAIL,
-    private_key: process.env.GOOGLE_SHEETS_PRIVATE_KEY.replace(/\\n/g, "\n"),
+    private_key: process.env.GOOGLE_SHEETS_PRIVATE_KEY.replace(/(\\r)|(\\n)/g, '\n'),
   });
 
   await doc.loadInfo(); // loads document properties and worksheets

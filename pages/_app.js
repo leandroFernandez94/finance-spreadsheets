63219b734f0690dba33afc2d5f6c8031156c1bff
Leandro Fernandez <leandroofernandezz@gmail.com>
2021-07-21 18:53:02 -0300

    add head tags

--- pages/_app.js
+++ pages/_app.js
@@ -3,13 +3,43 @@ import '../styles/global.css'
 
 export default function App({ Component, pageProps }) {
   return (
-    <SWRConfig
-      value={{
-        refreshInterval: 3000,
-        fetcher: (resource, init) => fetch(resource, init).then(res => res.json())
-      }}
-    >
-      <Component {...pageProps} />
-    </SWRConfig>
-  )
+    <>
+      <Head>
+        <meta charSet="utf-8" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+        <meta
+          name="viewport"
+          content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no"
+        />
+        <meta name="description" content="Track Personal Finances" />
+        <meta name="keywords" content="Keywords" />
+        <title>My Finances</title>
+
+        <link rel="manifest" href="/manifest.json" />
+        <link
+          href="/icons/favicon-16x16.png"
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+        />
+        <link
+          href="/icons/favicon-32x32.png"
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+        />
+        <link rel="apple-touch-icon" href="/apple-icon.png"></link>
+        <meta name="theme-color" content="#000000" />
+      </Head>
+      <SWRConfig
+        value={{
+          refreshInterval: 3000,
+          fetcher: (resource, init) =>
+            fetch(resource, init).then((res) => res.json()),
+        }}
+      >
+        <Component {...pageProps} />
+      </SWRConfig>
+    </>
+  );
 }
